db4d54f19b455c23010fa4e2d606f9d470b6675c
shane-smyth <158466904+shane-smyth@users.noreply.github.com>
2024-11-26 13:49:15 +0000

    reading & writing to file done for encryption

--- src/main/java/ca2/EncryptionUtil.java
+++ src/main/java/ca2/EncryptionUtil.java
@@ -3,6 +3,7 @@ package ca2;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.io.FileWriter;
 import java.util.Base64;
 
 public class EncryptionUtil {
@@ -17,7 +18,14 @@ public class EncryptionUtil {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedBytes);
+        String encryptedText = Base64.getEncoder().encodeToString(encryptedBytes);
+
+        // Writing to file https://www.geeksforgeeks.org/java-program-to-write-into-a-file/
+        FileWriter fileWriter = new FileWriter("cipherText.txt");
+        fileWriter.write(encryptedText);
+        fileWriter.close();
+
+        return encryptedText;
     }
 
     public static String decrypt(String cipherText, SecretKey secretKey) throws Exception {
--- src/main/java/ca2/Main.java
+++ src/main/java/ca2/Main.java
@@ -3,6 +3,8 @@ package ca2;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Base64;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -31,10 +33,13 @@ public class Main {
                     case 1:
                         System.out.println("Encrypt");
 
-                        System.out.println("enter text: ");
-                        String plaintext = keyboard.nextLine();
+                        System.out.println("enter file name: ");
+                        String fileName = keyboard.nextLine();
+                        fileName = TxtFileValidation.validateFileName(fileName);
+                        String plaintext = readFile(fileName);
 
                         System.out.println(EncryptionUtil.encrypt(plaintext, secretKey)+"\n\n");
+                        System.out.println(plaintext);
                         break;
                     case 2:
                         System.out.println("Decrypt");
@@ -51,5 +56,17 @@ public class Main {
             }
         }
         while (menuChoice != 0);
+    }
+
+    // https://www.geeksforgeeks.org/java-io-filereader-class/
+    public static String readFile(String fileName) throws Exception {
+        FileReader fileReader = new FileReader(fileName);
+        String fileContent = "";
+        int i;
+        while ((i = fileReader.read()) != -1) {
+            fileContent += (char)i;
+        }
+        fileReader.close();
+        return fileContent;
     }
 }
